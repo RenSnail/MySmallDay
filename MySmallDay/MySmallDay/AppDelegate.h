e405760f422f6feb66e29634e93f062ccaf53855
Snail <rensnail@gmail.com>
2016-03-15 13:58:13 +0800

    init

---
+++ MySmallDay/MySmallDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySmallDay
+//
+//  Created by Snail on 16/3/15.
+//  Copyright © 2016年 Snail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
